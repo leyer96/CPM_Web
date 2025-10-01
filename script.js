501be06a0fda909f88b5e22d96c2ecdc2fd96d16
Leo Arias <leoariase99@gmail.com>
2025-10-01 19:41:36 +0200

    Updated registration page

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-if (window.location.href.includes("register.html")) {
-    alert("PROMOCIONES:\n\nINSCRIPCIÓN A TALLER PEARS INCLUYE ACCESO A CONGRESO (SOLO Médicos Generales, Enfermeras, Residentes y Estudiantes) \n\nTALLER DE TOXICOLOGÍA: 50% DE DESCUENTO")
-}
+// if (window.location.href.includes("register.html")) {
+//     alert("PROMOCIONES:\n\nINSCRIPCIÓN A TALLER PEARS INCLUYE ACCESO A CONGRESO (SOLO Médicos Generales, Enfermeras, Residentes y Estudiantes) \n\nTALLER DE TOXICOLOGÍA: 50% DE DESCUENTO")
+// }
 
 document.getElementById("closeAd").addEventListener("click", function() {
     document.getElementById("adOverlay").style.display = "none";
